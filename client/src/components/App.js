6bb918ef2ea384ee91cae4fd90b53d9593e5be39
Olmos <siul_2010@live.com>
2024-07-25 17:15:26 -0600

    Renombré la carpeta de Client a client y agregué un icono

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Favicon from './favicon'; // Asegúrate de usar la ruta correcta
+
+function App() {
+  return (
+    <div className="App">
+        <Favicon />
+    </div>
+  );
+}
+
+export default App;
