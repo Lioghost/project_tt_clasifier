b64a0afbb7b06c2d6aec2ad073980318328237a2
Olmos <siul_2010@live.com>
2024-09-03 12:47:18 -0600

    Todo listo para implementar los dashboards

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from '../context/AuthContext'; // Importa el AuthProvider
 import Favicon from '../components/common/favicon'; 
 import Header from '../components/header/header';
 import MainView from '../components/mainview/mainview';
@@ -9,12 +8,9 @@ import Register from '../components/register/register';
 import ConfirmarCuenta from '../components/confirmarCuenta/confirmarCuenta'; // Importa el componente de confirmación
 import ForgotPassword from '../components/forgotPassword/forgotPassword'; // Importa el nuevo componente
 import ResetPassword from '../components/ResetPassword/resetPassword' // Importa el nuevo componente
-import DashboardAdmin from '../components/admin/dashboardAdmin'; // Asegúrate de tener este componente
-import DashboardClient from '../components/client/dashboardClient'; // Asegúrate de tener este componente
 
 function App() {
   return (
-    <AuthProvider> {/* Provee el contexto de autenticación */}
       <Router>
         <div className="App">
           <Favicon />
@@ -29,13 +25,9 @@ function App() {
             <Route path="/forgot-password" element={<><ForgotPassword /></>} />   {/* Nueva ruta para recuperar contraseña */}
             <Route path="/auth/olvide-password/:token" element={<ResetPassword />} /> {/* Nueva ruta para restablecer contraseña */}
     
-            {/* Rutas para los roles */}
-            <Route path="/admin/dashboard" element={<DashboardAdmin />} /> {/* Ruta protegida para Admin */}
-            <Route path="/client/dashboard" element={<DashboardClient />} /> {/* Ruta protegida para Cliente */}
           </Routes>
         </div>
       </Router>
-    </AuthProvider>
   );
 }
 
