4e840065e98462b318c34919d45a2e895cac5a38
Olmos <siul_2010@live.com>
2024-09-15 11:46:28 -0600

    Ajuste del nav del sidebard para que no cierre sesión

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,23 +1,41 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(() => {
+        // Inicializa el estado desde localStorage
+        return localStorage.getItem('isAuthenticated') === 'true';
+    });
+
+    const [role, setRole] = useState(() => {
+        return localStorage.getItem('role') || null;
+    });
+
     const navigate = useNavigate();
 
-    const login = () => {
+    useEffect(() => {
+        // Actualiza localStorage cuando cambia el estado de autenticación
+        localStorage.setItem('isAuthenticated', isAuthenticated);
+        localStorage.setItem('role', role);
+    }, [isAuthenticated, role]);
+
+    const login = (userRole) => {
         setIsAuthenticated(true);
+        setRole(userRole);
     };
 
     const logout = () => {
         setIsAuthenticated(false);
+        setRole(null);
+        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('role');
         navigate('/login');
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, role, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
